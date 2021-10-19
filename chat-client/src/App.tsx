4e498125adc16bcf573c39ad1de1b945f44401ac
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-19 21:21:08 +0200

    client is connected

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -1,16 +1,81 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 import chat from "./images/chat.png";
 import "./App.css";
 
+interface Props {
+    connection: any
+}
+interface State {
+    userId: string,
+    userName: string,
+    isClicked: boolean
+}
 
-export class App extends Component<{}, {}> {
-    // className='App-logo'
+export class App extends Component<Props, State> {
+    
+    state: State = {
+        userId: '',
+        userName: '',
+        isClicked: false
+    }
+
+    constructor(props: any) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    // create a ref to store the textInput DOM element
+    private userNameRef = createRef<HTMLInputElement>();
+
+    componentDidMount() {
+        if (this.props.connection) {
+            this.props.connection.start()
+            .then(() => {
+                var userId = this.props.connection.connectionId;
+                // console.log('User with userId: ', userId, ' is connected!');
+                this.setState({
+                    userId: userId,
+                    userName: 'Användarnamn'
+                });
+                //  console.log('State userId: ', this.state.userId);
+            })
+            .catch((error: any) => console.log('Connection failed: ', error))
+        }
+    }
+
+    handleChange() {
+        // console.log('userName: ', this.userNameRef.current!.value);
+        this.setState({
+            userName: this.userNameRef.current!.value
+        });
+    }
+
+    handleClick() {
+        this.setState({
+            isClicked: true
+        });
+        // console.log('State userId: ', this.state.userId, ' userName: ', this.state.userName, ' isClicked: ', this.state.isClicked);
+    }
+    
     render() {
-        return(
-            <div className='App-header'>
-                <h1>Välkommen till Hotell Mercer's chat!</h1>
-                <img src={chat} ></img>
-            </div>
-        )
+        if (!this.state.isClicked) {
+            return(
+                <div className='App-header'>
+                    <h1>Välkommen till Hotell Mercer's chat!</h1>
+                    <img src={chat} alt='chat icon' className='App-logo'></img>
+                    <p>Ange ditt användarnamn för att börja chatta:</p>
+                    <input ref={this.userNameRef} type='text' onChange={this.handleChange}/>
+                    <button onClick={this.handleClick}>Starta Chatten!</button>
+                </div>
+            )
+        }
+
+        else {
+            return(
+                <div className='App-header'>
+                    <h1>Chat</h1>  
+                </div>
+            )
+        }  
     }
 }
--- chat-client/src/index.tsx
+++ chat-client/src/index.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
+var connection;
+connectToServer();
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <App connection={connection}/>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+function connectToServer() {
+    connection = new HubConnectionBuilder()
+    .withUrl('https://localhost:5001/hubs/chat')
+    .withAutomaticReconnect()
+    .build();
+
+    return connection;
+}
