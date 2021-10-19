4a04a2c7cd61cf915ba142b58fa4a5cbe0d05148
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-19 18:06:44 +0200

    basic hub-setup

---
+++ Chat.Api/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Chat.Api.Hubs.Clients;
+using Chat.Api.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Api.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        //TODO: Implement Message
+        public async Task SendMessage(Message message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
--- Chat.Api/Startup.cs
+++ Chat.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chat.Api.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,18 @@ namespace Chat.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ClientPermission", policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials();
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -53,6 +66,7 @@ namespace Chat.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/hubs/chat");
             });
         }
     }
