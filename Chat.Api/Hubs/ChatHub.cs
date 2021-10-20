02eebad76317a6d041ba18d1548de8d6ae8af3d0
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-20 15:13:36 +0200

    added methods for joining/leaving groups(rooms)

--- Chat.Api/Hubs/ChatHub.cs
+++ Chat.Api/Hubs/ChatHub.cs
@@ -11,10 +11,37 @@ namespace Chat.Api.Hubs
 {
     public class ChatHub : Hub<IChatClient>
     {
-        //TODO: Implement Message
         public async Task SendMessage(ChatMessage message)
         {
             await Clients.All.ReceiveMessage(message);
+        }
+
+        public async Task JoinRoom(string connectionId, string roomName)
+        {
+            await Groups.AddToGroupAsync(connectionId, roomName);
+
+            var message = new ChatMessage
+            {
+                UserId = connectionId,
+                RoomName = roomName,
+                Message = $"{connectionId} has joined the group {roomName}."
+            };
+
+            await Clients.Group(roomName).ReceiveMessage(message);
+        }
+
+        public async Task LeaveRoom(string connectionId, string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(connectionId, roomName);
+
+            var message = new ChatMessage
+            {
+                UserId = connectionId,
+                RoomName = roomName,
+                Message = $"{connectionId} has left the group {roomName}."
+            };
+
+            await Clients.Group(roomName).ReceiveMessage(message);
         }
     }
 }
