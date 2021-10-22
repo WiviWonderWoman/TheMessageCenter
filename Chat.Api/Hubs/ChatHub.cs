949ca3c06616eb10e90f41e1fd6eb2a088a722b6
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-22 17:12:49 +0200

    Lagesrapport (#20)
    * cleaned up unused code & Update README.md

--- Chat.Api/Hubs/ChatHub.cs
+++ Chat.Api/Hubs/ChatHub.cs
@@ -1,10 +1,5 @@
-﻿using Chat.Api.Hubs.Clients;
-using Chat.Api.Models;
+﻿using Chat.Api.Models;
 using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Chat.Api.Hubs
