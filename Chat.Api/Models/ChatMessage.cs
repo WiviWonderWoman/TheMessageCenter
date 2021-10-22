761be7c969f8436a7f9a1bc011a76f4b7ea4c27a
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-22 16:55:31 +0200

    cleaned up unused code

--- Chat.Api/Models/ChatMessage.cs
+++ Chat.Api/Models/ChatMessage.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chat.Api.Models
+﻿namespace Chat.Api.Models
 {
     public class ChatMessage
-    {
-        //public string UserId { get; set; }
-        //public string RoomName { get; set; }
+    {       
         public string User { get; set; }
         public string Message { get; set; }
     }
--- Chat.Api/Models/ChatRoom.cs
+++ Chat.Api/Models/ChatRoom.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chat.Api.Models
+﻿namespace Chat.Api.Models
 {
     public class ChatRoom
     {
         public string RoomName { get; set; }
-
-        //public int RoomId { get; set; }
     }
 }
