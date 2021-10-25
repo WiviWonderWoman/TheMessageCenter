15eb61c38d00b545f5dd1079acf24d1272a71ba2
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-25 21:06:52 +0200

    added apifolder, caller and get rooms. And displaying rooms and joining standard room in chat

---
+++ chat-client/src/api/room.js
@@ -0,0 +1,19 @@
+import Caller from './caller';
+
+export async function getChatRooms() {
+    try {
+        const response = await Caller.get();
+
+        if (response.status !== 200) {
+            throw Error(response.statusText);
+        }
+        var rooms = [{}]
+        rooms = response.data;
+        console.log('Från rooms.js: ', response.data)
+        return rooms;
+    } 
+    catch (error) {
+        console.log(error);
+        return error;
+    }
+}
