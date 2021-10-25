e9faf2d59b23550111892f7536efb6a860314f37
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-25 14:37:39 +0200

    Room repository (#21)
    * added repository and mock-implementation

---
+++ Chat.Api/Repositories/MockChatRoomRepository.cs
@@ -0,0 +1,21 @@
+﻿using Chat.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Api.Repositories
+{
+    // Mock implementation of IChatRoomRepository with hardcoded data (reaplace with database)
+    public class MockChatRoomRepository : IChatRoomRepository
+    {
+        public IEnumerable<ChatRoom> AllChatRooms => new List<ChatRoom>
+        {
+            new ChatRoom {RoomName = "Allmänt"},
+            new ChatRoom {RoomName = "Reception"},
+            new ChatRoom {RoomName = "Kök"},
+            new ChatRoom {RoomName = "Städ"}
+        };
+    }
+}
