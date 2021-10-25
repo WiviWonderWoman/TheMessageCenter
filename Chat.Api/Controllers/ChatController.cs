e4e5e956457ba2da7c5c276a4012df74e6798bf2
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-10-25 16:00:35 +0200

    chat-controller GET /chat/rooms returns standard chatrooms (#22)

---
+++ Chat.Api/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Chat.Api.Models;
+using Chat.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IChatRoomRepository chatRoomRepository;
+
+        public ChatController(IChatRoomRepository _chatRoomRepository)
+        {
+            chatRoomRepository = _chatRoomRepository;
+        }
+
+        [HttpGet("rooms")]
+        public IEnumerable<ChatRoom> GetRooms()
+        {
+            return chatRoomRepository.AllChatRooms;
+        }
+    }
+}
